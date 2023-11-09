f6a9e515f70eac04cd282eb1a5cb43ed3301b130
lazy <ddlazy@qq.com>
2023-11-09 14:16:02 +0800

    first

---
+++ src/pages/index/Row.vue
@@ -0,0 +1,14 @@
+<template>
+    <view :id="id" :class="index % 2 ? 'ListItemOdd' : 'ListItemEven'">
+        Row {{ index }} : {{ data[index] }}
+    </view>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    id: any;
+    index: any;
+    data: any;
+    isScrolling: any;
+}>();
+</script>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <VirtualList
+        class="List"
+        :height="500"
+        :item-data="list"
+        :item-count="list.length"
+        :item-size="100"
+        :item="Row"
+        width="100%"
+    />
+</template>
+
+<script setup lang="ts">
+import { ref, markRaw } from "vue";
+import Rows from "./Row.vue";
+import VirtualList from "@tarojs/components/virtual-list";
+const buildData = (offset = 0) => {
+    return Array(100)
+        .fill(0)
+        .map((_, i) => i + offset);
+};
+const Row = markRaw(Rows);
+const list = ref(buildData(0));
+console.log(list.value);
+
+</script>
